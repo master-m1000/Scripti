9d3c88b17eab5e1622cb814e07c43736cc8f98a2
master-m1000 <mario.wagenknecht@gmail.com>
2016-02-11 06:16:39 +0100

    Added me.license Shows the license

--- Scripti/Scripti/ModInterpreter.vb
+++ Scripti/Scripti/ModInterpreter.vb
@@ -76,6 +76,28 @@
             Case "color"
                 Console.BackgroundColor = CType(lineRegEx(0).Result("$3"), ConsoleColor)
                 Console.ForegroundColor = CType(lineRegEx(0).Result("$4"), ConsoleColor)
+            Case "license"
+                Console.WriteLine("The MIT License (MIT)")
+                Console.WriteLine("")
+                Console.WriteLine("Copyright (c) 2016 Mario Wagenknecht")
+                Console.WriteLine("")
+                Console.WriteLine("Permission is hereby granted, free of charge, to any person obtaining a copy")
+                Console.WriteLine("of this software and associated documentation files (the ""Software""), to deal")
+                Console.WriteLine("in the Software without restriction, including without limitation the rights")
+                Console.WriteLine("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell")
+                Console.WriteLine("copies of the Software, and to permit persons to whom the Software is")
+                Console.WriteLine("furnished to do so, subject to the following conditions:")
+                Console.WriteLine("")
+                Console.WriteLine("The above copyright notice and this permission notice shall be included in all")
+                Console.WriteLine("copies or substantial portions of the Software.")
+                Console.WriteLine("")
+                Console.WriteLine("THE SOFTWARE IS PROVIDED ""As Is"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
+                Console.WriteLine("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+                Console.WriteLine("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+                Console.WriteLine("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
+                Console.WriteLine("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
+                Console.WriteLine("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE")
+                Console.WriteLine("SOFTWARE.")
             Case "pause"
                 Pause()
             Case "resetcolor"
